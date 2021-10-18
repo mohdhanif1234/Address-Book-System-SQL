ba7faafe0cd4d0d34e1baa59a7090bdb1ace0eef
mohdhanif1234 <hanif.topia@gmail.com>
2021-10-18 17:14:16 +0530

    [Hanif]:Added a SQL query to insert new contacts in address book

--- AddressBookServiceSQL.sql
+++ AddressBookServiceSQL.sql
@@ -14,3 +14,11 @@ ZipCode int,
 Phonenum bigint,
 EmailId varchar(100)
 );
+
+--UC-3: Inserting new contacts in the address book
+insert into AddressBookTable values
+('Alfreds','Futterkiste','Obere Str. 57','Berlin','Germany',764842,458721564,'abc12@gmail.com'),
+('Ana','Trujillo','Avda. de la Constitution 2222','Mexico','USA',569142,3587215654,'pqr14@gmail.com'),
+('Antonio','Moreno','Mataderos 2312','London','England',761942,258721564,'xyz12@gmail.com'),
+('Thomas','Hardy','120 Hanover Sq.','Lulea','Sweden',231042,458721564,'def12@gmail.com');
+select * from AddressBookTable;
