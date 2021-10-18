9565b28b6945993a8c0f4cb776647165e541d871
mohdhanif1234 <hanif.topia@gmail.com>
2021-10-18 16:59:55 +0530

    [Hanif]:Added a SQL query to create a DB table

--- AddressBookServiceSQL.sql
+++ AddressBookServiceSQL.sql
@@ -1,3 +1,16 @@
 --UC-1: Creating address book service
 create database AddressBookService;
 use AddressBookService;
+
+--UC-2: Creating database table
+create table AddressBookTable
+(
+FirstName varchar(100) not null,
+LastName varchar(50),
+Address varchar(200),
+City varchar(150),
+StateName varchar(100),
+ZipCode int,
+Phonenum bigint,
+EmailId varchar(100)
+);
